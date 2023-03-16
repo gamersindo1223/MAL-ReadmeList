31937414c599d8408373c210d94c58a90d1e2146
Gamers_indo1223 <76719536+gamersindo1223@users.noreply.github.com>
2023-03-16 05:33:59 +0000

    Intiial Commit

---
+++ index.mjs
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import * as fetcher from './lib/Fetcher.mjs'
+import axios from 'axios'
+const git = import("korefile")
+
+async function init() {
+    let username = process.env.username
+    let gh_token = process.env.gh_token
+    let readme_path = process.env.readme_path
+    let branch = process.env.branch
+    let limit = process.env.limit
+    let list;
+    console.log(await fetcher.fetchAnime("finished"))
+    /*
+    const fetchAnimeList = async (status) => {
+        const url = `https://myanimelist.net/animelist/${username}/load.json?status=${status}`;
+        const response = await fetch(url).catch(err => {throw new Error(err)})
+        const animeList = await response.json();
+        return animeList
+      };
+      const anime_watching = await fetchAnimeList('1')
+      for(let i = 0; i < anime_watching.length; i++) {
+        
+      }
+      */
+}
+init()
---
+++ test.mjs
@@ -0,0 +1,6 @@
+const min = 1,
+      max = 100
+
+      console.log(Math.floor(
+        Math.random() * (max - min + 1) + min
+      ))
