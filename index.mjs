3b076bd2fc868eaa6587e35f31393c79887903a0
Gamers_indo1223 <76719536+gamersindo1223@users.noreply.github.com>
2023-03-26 03:45:22 +0000

    Version 1.0 (Stable Version)

--- index.mjs
+++ index.mjs
@@ -2,30 +2,31 @@ import dotenv from 'dotenv'
 dotenv.config()
 import * as fetcher from './lib/Fetcher.mjs'
 import {createKoreFile, createGitHubAdaptor} from "korefile";
-
-
 async function init() {
+    let filename = process.env.filename || "README.md"
     let username = process.env.username
     let gh_token = process.env.gh_token
     let readme_path = process.env.readme_path
     let branch = process.env.branch
     let data = await fetcher.fetchAnime(username)
-    let currentreadme = await fetcher.getreadme(readme_path, gh_token, branch)
-    let completednewreadme = await fetcher.appendAnimeCompleted(currentreadme, data.completed)
-    let watchingnewreadme = await fetcher.appendAnimeWatching(completednewreadme, data.watching)
-    let ptwnewreadme = await fetcher.appendAnimeCompleted(watchingnewreadme, data.ptw)
-    console.log(completednewreadme)
+    let currentreadme = await fetcher.getreadme(readme_path, gh_token, branch, filename)
+    let newreadme = await fetcher.appendAnime(currentreadme, data)
+    if(newreadme === currentreadme){
+        console.log("No new entries")
+        return process.exit(1)
+    }
+    
+    console.log("New Changes Detected!")
+    console.log("Pushing to Github")
+    let file = createKoreFile({
+        adaptor: createGitHubAdaptor({
+            owner: readme_path.split("/")[0],
+            repo: readme_path.split("/")[1],
+            ref: `heads/${branch}`,
+            token: gh_token
+        })
+    });
+    file.writeFile(filename, newreadme);
+    console.log(`${filename} Updated, Ending Process...`)
 }
 init()
-    /*
-    const fetchAnimeList = async (status) => {
-        const url = `https://myanimelist.net/animelist/${username}/load.json?status=${status}`;
-        const response = await fetch(url).catch(err => {throw new Error(err)})
-        const animeList = await response.json();
-        return animeList
-      };
-      const anime_watching = await fetchAnimeList('1')
-      for(let i = 0; i < anime_watching.length; i++) {
-        
-      }
-      */
